c69194406a662324bf880a3a124320a1639d0917
Aetiras <aetirascr@outlook.com>
2024-07-15 17:14:53 +0300

    feat: main -> Bug Fixes

--- example/example.dart
+++ example/example.dart
@@ -1,7 +1,7 @@
 import 'package:cc_rest_api/api/modules/api_module.dart';
 
-class GetUser extends CCApiModule {
-  GetUser(super.config);
+class Example extends CCApiModule {
+  Example(super.config);
 
   @override
   Future<Map<String, dynamic>> request() async {
